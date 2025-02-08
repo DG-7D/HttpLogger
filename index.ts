4fb13181aec47b365a55dd8ddc8372bfd6378c41
DG-7D <96519093+DG-7D@users.noreply.github.com>
2025-02-09 00:13:18 +0900

    偉業のBun

--- index.ts
+++ index.ts
@@ -1,31 +1,30 @@
-import { promises as fs } from "fs";
+import type { FileSink } from "bun";
 
 let keys: string[] = [];
-let csvFile: fs.FileHandle | undefined;
+let writer: FileSink | undefined;
 
-async function openNewLog() {
-    csvFile?.close();
+function openNewLog() {
+    writer?.end();
     const isoString = new Date().toISOString();
     const filename = "./_csv/" + isoString.substring(0, 4) + isoString.substring(5, 7) + isoString.substring(8, 10) + isoString.substring(11, 13) + isoString.substring(14, 16) + isoString.substring(17, 19) + ".csv";
     console.write("Logging to " + filename + "\n");
-    csvFile = await fs.open(filename, "w+");
+    writer = Bun.file(filename).writer();
 }
 
 function writeLog(data: string) {
     console.write(data);
-    return csvFile?.write(data);
+    writer?.write(data);
 }
 
-await fs.mkdir("_csv");
 const server = Bun.serve({
-    async fetch(request) {
+    fetch(request) {
         const params = new URL(request.url).searchParams;
         if ([...params.keys()].length !== keys.length || !keys.every(key => params.has(key))) {
             keys = [...params.keys()];
-            await openNewLog();
-            await writeLog(keys.join(",") + "\n");
+            openNewLog();
+            writeLog(keys.join(",") + "\n");
         }
-        await writeLog(
+        writeLog(
             keys.map(key => params.get(key) ?? "").join(",") + "\n"
         );
         return new Response();
